cbf47af24c9e50032f53fb46c578e838b978df3c
Rodolfo Moreira <rodolfomoreira1994@gmail.com>
2021-05-23 10:28:31 -0300

    Initial Commit

---
+++ src/views/Credencials/Index.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Aqui é Credencials</h1>
+</template>
+<script>
+export default {
+}
+</script>
---
+++ src/views/Feedbacks/index.vue
@@ -0,0 +1,7 @@
+<template >
+    <h1>Aqui é o Feedbacks</h1>
+</template>
+<script>
+export default {
+}
+</script>
