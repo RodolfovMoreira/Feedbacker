e5162434878a41b3cb84e8ee552ab694473e86fa
Rodolfo Moreira <rodolfomoreira1994@gmail.com>
2021-05-25 00:09:48 -0300

    Feedback Screen - done - Loading effects added

--- src/components/Icon/index.vue
+++ src/components/Icon/index.vue
@@ -4,10 +4,11 @@
 
 <script>
 import Loading from './Loading.vue'
-import Copy from './Copy'
+import Copy from './Copy.vue'
+import ChevronDown from './ChevronDown.vue'
 
 export default {
-  components: { Loading, Copy },
+  components: { Loading, Copy, ChevronDown },
   props: {
     name: { type: String, required: true }
   }
